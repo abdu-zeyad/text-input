972a78c6f1d71aa1a12a55f0ff49012df8b4232d
Abelmajeed Alabedalraheem <abd.d@live.com>
2024-07-03 01:36:30 +0300

    s

--- react-native-material-textfield/src/components/affix/index.tsx
+++ react-native-material-textfield/src/components/affix/index.tsx
@@ -1,65 +1,67 @@
-import PropTypes from "prop-types";
-import React, { PureComponent } from "react";
-import { Animated, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextProps,
+  Animated,
+  StyleSheet,
+  ViewProps,
+  TextStyle,
+  ViewStyle,
+} from "react-native";
+import React from "react";
+import { type PropsWithChildren } from "react";
 
-export default class Affix extends PureComponent {
-  static defaultProps = {
-    numberOfLines: 1,
-  };
-
-  static propTypes = {
-    numberOfLines: PropTypes.number,
-    style: Animated.Text.propTypes.style,
-
-    color: PropTypes.string.isRequired,
-    fontSize: PropTypes.number.isRequired,
-
-    type: PropTypes.oneOf(["prefix", "suffix"]).isRequired,
-
-    labelAnimation: PropTypes.instanceOf(Animated.Value).isRequired,
-
-    children: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.node),
-      PropTypes.node,
-    ]),
-  };
-
-  render() {
-    let { labelAnimation, style, children, type, fontSize, color } = this.props;
-
-    let containerStyle = {
-      height: fontSize * 1.5,
-      opacity: labelAnimation,
-    };
-
-    let textStyle = {
-      includeFontPadding: false,
-      textAlignVertical: "top",
-
-      fontSize,
-      color,
-    };
-
-    switch (type) {
-      case "prefix":
-        containerStyle.paddingRight = 8;
-        textStyle.textAlign = "left";
-        break;
-
-      case "suffix":
-        containerStyle.paddingLeft = 8;
-        textStyle.textAlign = "right";
-        break;
-    }
-
-    return (
-      <Animated.View style={[styles.container, containerStyle]}>
-        <Animated.Text style={[style, textStyle]}>{children}</Animated.Text>
-      </Animated.View>
-    );
-  }
+interface AffixProps {
+  numberOfLines: number;
+  style: TextStyle;
+  color: string;
+  fontSize: number;
+  type: "prefix" | "suffix";
+  labelAnimation: Animated.Value;
 }
 
+const Affix = ({
+  numberOfLines = 1,
+  style,
+  color,
+  fontSize,
+  children,
+  type,
+  labelAnimation,
+}: PropsWithChildren<AffixProps>) => {
+  let containerStyle: ViewStyle = {
+    height: fontSize * 1.5,
+    opacity: labelAnimation,
+  };
+
+  let textStyle: TextStyle = {
+    includeFontPadding: false,
+    textAlignVertical: "top",
+
+    fontSize,
+    color,
+  };
+
+  switch (type) {
+    case "prefix":
+      containerStyle.paddingRight = 8;
+      textStyle.textAlign = "left";
+      break;
+
+    case "suffix":
+      containerStyle.paddingLeft = 8;
+      textStyle.textAlign = "right";
+      break;
+  }
+
+  return (
+    <Animated.View style={[styles.container, containerStyle]}>
+      <Animated.Text style={[style, textStyle]}>{children}</Animated.Text>
+    </Animated.View>
+  );
+};
+
+export default Affix;
 const styles = StyleSheet.create({
   container: {
     top: 2,
