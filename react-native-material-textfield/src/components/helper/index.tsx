36a02adf33b7d2d63d7ebc105c688b8010d362b0
Abelmajeed Alabedalraheem <abd.d@live.com>
2024-07-03 03:15:33 +0300

    animate

--- react-native-material-textfield/src/components/helper/index.tsx
+++ react-native-material-textfield/src/components/helper/index.tsx
@@ -1,97 +1,78 @@
-import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import { Animated } from 'react-native';
+import PropTypes from "prop-types";
+import React, { PureComponent, useState } from "react";
+import { Animated, StyleSheet, TextStyle } from "react-native";
 
-import styles from './styles';
+interface HelperProps {
+  title: string;
+  error: string;
 
-export default class Helper extends PureComponent {
-  static propTypes = {
-    title: PropTypes.string,
-    error: PropTypes.string,
+  disabled: boolean;
 
-    disabled: PropTypes.bool,
+  style: TextStyle;
 
-    style: Animated.Text.propTypes.style,
+  baseColor: string;
+  errorColor: string;
 
-    baseColor: PropTypes.string,
-    errorColor: PropTypes.string,
-
-    focusAnimation: PropTypes.instanceOf(Animated.Value),
-  };
-
-  constructor(props) {
-    super(props);
-
-    let { error, focusAnimation } = this.props;
-
-    let opacity = focusAnimation.interpolate({
-      inputRange: [-1, -0.5, 0],
-      outputRange: [1, 0, 1],
-      extrapolate: 'clamp',
-    });
-
-    this.state = {
-      errored: !!error,
-      opacity,
-    };
-  }
-
-  componentDidMount() {
-    let { focusAnimation } = this.props;
-
-    this.listener = focusAnimation
-      .addListener(this.onAnimation.bind(this));
-  }
-
-  componentWillUnmount() {
-    let { focusAnimation } = this.props;
-
-    focusAnimation.removeListener(this.listener);
-  }
-
-  onAnimation({ value }) {
-    if (this.animationValue > -0.5 && value <= -0.5) {
-      this.setState({ errored: true });
-    }
-
-    if (this.animationValue < -0.5 && value >= -0.5) {
-      this.setState({ errored: false });
-    }
-
-    this.animationValue = value;
-  }
-
-  render() {
-    let { errored, opacity } = this.state;
-    let {
-      style,
-      title,
-      error,
-      disabled,
-      baseColor,
-      errorColor,
-    } = this.props;
-
-    let text = errored?
-      error:
-      title;
-
-    if (null == text) {
-      return null;
-    }
-
-    let textStyle = {
-      opacity,
-
-      color: !disabled && errored?
-        errorColor:
-        baseColor,
-    };
-
-    return (
-      <Animated.Text style={[styles.text, style, textStyle]}>
-        {text}
-      </Animated.Text>
-    );
-  }
+  focusAnimation: Animated.Value;
 }
+
+const Helper = ({
+  title,
+  error,
+  disabled,
+  style,
+  baseColor,
+  errorColor,
+  focusAnimation,
+}: HelperProps) => {
+  let opacityInitial = focusAnimation.interpolate({
+    inputRange: [-1, -0.5, 0],
+    outputRange: [1, 0, 1],
+    extrapolate: "clamp",
+  });
+
+  const [errored, setErrored] = useState(!!error);
+  const [opacity, setOpacity] = useState(opacityInitial);
+
+  // const onAnimation = ({ value }) => {
+  //   if (animationValue > -0.5 && value <= -0.5) {
+  //     this.setState({ errored: true });
+  //   }
+
+  //   if (this.animationValue < -0.5 && value >= -0.5) {
+  //     this.setState({ errored: false });
+  //   }
+
+  //   this.animationValue = value;
+  // };
+
+  let text = errored ? error : title;
+
+  if (null == text) {
+    return null;
+  }
+
+  let textStyle = {
+    opacity,
+
+    color: !disabled && errored ? errorColor : baseColor,
+  };
+  return (
+    <Animated.Text style={[styles.text, style, textStyle]}>
+      {text}
+    </Animated.Text>
+  );
+};
+
+export default Helper;
+
+const styles = StyleSheet.create({
+  text: {
+    flex: 1,
+    fontSize: 12,
+    lineHeight: 16,
+    backgroundColor: "transparent",
+    paddingVertical: 2,
+    textAlign: "left",
+  },
+});
