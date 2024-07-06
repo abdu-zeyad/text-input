0231746bc5374d63230faaf588055b79450b8b03
Abelmajeed Alabedalraheem <abd.d@live.com>
2024-07-07 00:51:08 +0300

    add types

---
+++ TextInput/others.ts
@@ -0,0 +1,120 @@
+type Mode = "adaptive" | "exact";
+
+export type ThemeBase = {
+  dark: boolean;
+  mode?: Mode;
+  roundness: number;
+  animation: {
+    scale: number;
+    defaultAnimationDuration?: number;
+  };
+};
+export enum ElevationLevels {
+  "level0",
+  "level1",
+  "level2",
+  "level3",
+  "level4",
+  "level5",
+}
+
+export type MD3ElevationColors = {
+  [key in keyof typeof ElevationLevels]: string;
+};
+
+export type MD3Colors = {
+  primary: string;
+  primaryContainer: string;
+  secondary: string;
+  secondaryContainer: string;
+  tertiary: string;
+  tertiaryContainer: string;
+  surface: string;
+  surfaceVariant: string;
+  surfaceDisabled: string;
+  background: string;
+  error: string;
+  errorContainer: string;
+  onPrimary: string;
+  onPrimaryContainer: string;
+  onSecondary: string;
+  onSecondaryContainer: string;
+  onTertiary: string;
+  onTertiaryContainer: string;
+  onSurface: string;
+  onSurfaceVariant: string;
+  onSurfaceDisabled: string;
+  onError: string;
+  onErrorContainer: string;
+  onBackground: string;
+  outline: string;
+  outlineVariant: string;
+  inverseSurface: string;
+  inverseOnSurface: string;
+  inversePrimary: string;
+  shadow: string;
+  scrim: string;
+  backdrop: string;
+  elevation: MD3ElevationColors;
+};
+// MD3 types
+export enum MD3TypescaleKey {
+  displayLarge = "displayLarge",
+  displayMedium = "displayMedium",
+  displaySmall = "displaySmall",
+
+  headlineLarge = "headlineLarge",
+  headlineMedium = "headlineMedium",
+  headlineSmall = "headlineSmall",
+
+  titleLarge = "titleLarge",
+  titleMedium = "titleMedium",
+  titleSmall = "titleSmall",
+
+  labelLarge = "labelLarge",
+  labelMedium = "labelMedium",
+  labelSmall = "labelSmall",
+
+  bodyLarge = "bodyLarge",
+  bodyMedium = "bodyMedium",
+  bodySmall = "bodySmall",
+}
+export type Font = {
+  fontFamily: string;
+  fontWeight?:
+    | "normal"
+    | "bold"
+    | "100"
+    | "200"
+    | "300"
+    | "400"
+    | "500"
+    | "600"
+    | "700"
+    | "800"
+    | "900";
+  fontStyle?: "normal" | "italic" | undefined;
+};
+
+export type MD3Type = {
+  fontFamily: string;
+  letterSpacing: number;
+  fontWeight: Font["fontWeight"];
+  lineHeight: number;
+  fontSize: number;
+  fontStyle?: Font["fontStyle"];
+};
+export type MD3Typescale =
+  | {
+      [key in MD3TypescaleKey]: MD3Type;
+    } & {
+      ["default"]: Omit<MD3Type, "lineHeight" | "fontSize">;
+    };
+export type MD3Theme = ThemeBase & {
+  version: 3;
+  isV3: true;
+  colors: MD3Colors;
+  fonts: MD3Typescale;
+};
+
+export type InternalTheme = MD3Theme;
